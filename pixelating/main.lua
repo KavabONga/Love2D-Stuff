6a85ff01289c353fbddf232a45732d7fb3e1ae8a
Dmitriy Shabat <mitya.shabat@gmail.com>
2018-12-11 10:38:18 +0300

    Added a pixelating pixel shader

---
+++ pixelating/main.lua
@@ -0,0 +1,46 @@
+local kirby
+
+local defaultShader = [[
+	uniform vec2 resolution;
+	vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords )
+    {
+    	vec4 texcolor = Texel(texture, floor(texture_coords * resolution) / resolution);
+        return texcolor * color;
+    }
+]]
+
+local shader
+local time
+local res = 3
+
+function love.load()
+	time = 0
+	kirby = love.graphics.newImage("Kirby.png")
+	love.window.setMode(kirby:getWidth(), kirby:getHeight(), {
+		fullscreen = false,
+		resizable = false,
+		vsync = true
+	})
+	shader = love.graphics.newShader(defaultShader)
+end
+
+function love.update(dt)
+	time = time + dt
+end
+
+function love.draw()
+	love.graphics.setShader(shader)
+	shader:send("resolution", {res, res})
+	love.graphics.draw(kirby, 0, 0)
+	love.graphics.setShader()
+	love.graphics.setNewFont(24)
+	love.graphics.printf("Scroll", 0, 0, love.graphics.getWidth(), "center")
+end
+
+function love.wheelmoved(x, y)
+	if y > 0 then
+		res = res + 1
+	elseif y < 0 then
+		res = math.max(res - 1, 1)
+	end
+end
